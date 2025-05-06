10ef8e7840315b65590df907fd684323fd74b139
Lucas Sainte-croix <lucas.stx@mailo.com>
2025-05-06 15:12:20 +0200

    css et accueil

--- dossier_REACT/src/pages/PageAccueil.jsx
+++ dossier_REACT/src/pages/PageAccueil.jsx
@@ -5,10 +5,12 @@ const PageAccueil = () => {
     return (
         <div className="accueil-container">
             <h1 className="titre"> Bienvenue sur Pronote 2.0 !</h1>
-            <h2 className="sous-titre"> Connexion </h2>
-            <div className="boutons-groupés">
-                <button className="bouton-login">Élève</button>
-                <button className="bouton-login">Professeur·e</button>
+            <div className="contenu-central">
+                <h2 className="sous-titre">Connexion</h2>
+                <div className="boutons-groupés">
+                    <button className="bouton-login">Élève</button>
+                    <button className="bouton-login">Professeur·e</button>
+                </div>
             </div>
         </div>
     );
