09abd1437759aec96baf8ef64e832a3120a82f0b
jzitounifla <jzitounifla@ensc.fr>
2025-05-07 11:16:31 +0200

    PageProfNotes contenu -> menus déroulants à compléter

--- dossier_REACT/src/App.jsx
+++ dossier_REACT/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import React from 'react';
 import PageAccueil from "./pages/PageAccueil.jsx";
+import PageProfNote from './pages/PageProfNote.jsx';
 
 function App() {
   return <PageAccueil />;
