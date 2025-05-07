cf783b5b229741d7841b66a16f5a8153d6a4f511
qlopez <qlopez@ensc.fr>
2025-05-07 09:53:21 +0200

    Nouvelle architecture

---
+++ dossier_REACT/src/pages/PageProfNote.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../style/PageProfNote.css";
+
+const PageProfNote = () => {
+    return (
+        <div className="accueil-container">
+            <h1 className="titre"> Bienvenue sur Pronote 2.0 !</h1>
+            <div className="contenu-central">
+                <h2 className="sous-titre">Connexion</h2>
+                <div className="boutons-groupés">
+                    <button className="bouton-login">Élève</button>
+                    <button className="bouton-login">Professeur·e</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default PageProfNote;
